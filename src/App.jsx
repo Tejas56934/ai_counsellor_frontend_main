c5032091640cb1ab4ff7fb32b92676c2a2dd7be4
Tejas Pandit <tejaspandit623@gmail.com>
2025-11-26 11:32:37 +0530

    changes on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Sidebar from './components/sidebar.jsx';
+import Sidebar from './components/sidebar';
 import AdmissionWizard from "./components/AdmissionWizard";
 
 
