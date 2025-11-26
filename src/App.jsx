bfe8bd99a427ceafa5d0394042c7d04966ee66bd
Tejas Pandit <tejaspandit623@gmail.com>
2025-11-26 11:42:37 +0530

    fix: case-sensitive import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Sidebar from './components/sidebar';
+import Sidebar from "./components/sidebar.jsx";
+
 import AdmissionWizard from "./components/AdmissionWizard";
 
 
